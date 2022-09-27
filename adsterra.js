bf65978d32d13ff8354261ecabad10126ccabf9b
agcwall <111549581+agcwall@users.noreply.github.com>
2022-09-27 23:01:54 +0700

    hajar
    gasken 1M

---
+++ adsterra.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://recedewell.com/mz2t93v4u?key=81f79850f12b6899c308c1d201d23211', '_blank');
+	});
